df7d726ceee80e214322039e6888330d12a0b6f1
Farhad <i.m.farhad999@gmail.com>
2021-10-21 20:11:59 +0600

    product props update

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -10,24 +10,26 @@ import {
   } from "react-router-dom";
 
 
-const Product = (props) => {
+const Product = ({product,handleAddProduct,showAddToCart}) => {
+    // const {product,handleAddProduct}=props
+
     // console.log(props)
     // console.log(props.product)
-    const {seller,price,stock}=props.product;
+    const {seller,price,stock}=product;
     return (
         <div className="product">
             <div>
-                <img src={props.product.img} alt="" />
+                <img src={product.img} alt="" />
             </div>
             <div>
-                <h4 className="product-name"><Link to={"/product/"+props.product.key}>{props.product.name}</Link></h4>
+                <h4 className="product-name"><Link to={"/product/"+product.key}>{product.name}</Link></h4>
                 <br />
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in stock-Order soon</small></p>  
                 {
-                    props.showAddToCart===true &&
-                    <button onClick={()=>props.handleAddProduct(props.product)} className="add-to-cart-btn"><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
+                    showAddToCart===true &&
+                    <button onClick={()=>handleAddProduct(product)} className="add-to-cart-btn"><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
                 }
             </div>
         </div>
